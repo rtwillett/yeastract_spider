f206203a8ebc0bca76db5d73733597cf80feaf78
rtwillett <ryan.willett@gmail.com>
2019-05-03 23:24:46 -0400

    Fixing broken column call for gene list

--- yeastract/spiders/yeastract_spider.py
+++ yeastract/spiders/yeastract_spider.py
@@ -10,28 +10,38 @@ class Yeastract_Spider(Spider):
     name = "yeastract"
     allowed_urls = ["http://www.yeastract.com"]
 
-    df_genes = pd.read_csv("labels_BP.csv", usecols = ["genes"])
-    all_genes = df_genes["genes"].tolist()
+    df_genes = pd.read_csv("labels_BP.csv", usecols = ["gene"])
+    all_genes = df_genes["gene"].tolist()
 
     start_urls = [url_stub + i for i in all_genes]
 
 
     def parse(self, response):
 
+        #sysname = ""
         proteinname = response.xpath('//td[@property="yontology:proteinName_"]/text()').extract_first()
         description = response.xpath('//td[@property="yontology:description_"]/text()').extract_first()
 
         # Joining list with : demarcators
-        go_BioProc = response.xpath('//table[@summary="main content"]//table/tr[1]//a/text()').extract()
-        go_BioProc = (":").join(go_BioProc)
+        try:
+            go_BioProc = response.xpath('//table[@summary="main content"]//table/tr[1]//a/text()').extract()
+            go_BioProc = (":").join(go_BioProc)
+        except:
+            go_BioProc = "Test"
 
         # Joining list with : demarcators
-        go_MolFunc = response.xpath('//table[@summary="main content"]//table/tr[2]//a/text()').extract()
-        go_MolFunc = (":").join(go_MolFunc)
+        try:
+            go_MolFunc = response.xpath('//table[@summary="main content"]//table/tr[2]//a/text()').extract()
+            go_MolFunc = (":").join(go_MolFunc)
+        except:
+            go_MolFunc = "Test"
 
         # Joining list with : demarcators
-        go_CellComp = response.xpath('//table[@summary="main content"]//table/tr[3]//a/text()').extract()
-        go_CellComp = (":").join(go_CellComp)
+        try:
+            go_CellComp = response.xpath('//table[@summary="main content"]//table/tr[3]//a/text()').extract()
+            go_CellComp = (":").join(go_CellComp)
+        except:
+            go_CellComp = "Test"
 
         print(proteinname)
         print("="*55)
@@ -52,5 +62,5 @@ class Yeastract_Spider(Spider):
         item["go_BioProc"] = go_BioProc
         item["go_MolFunc"] = go_MolFunc
         item["go_CellComp"] = go_CellComp
-        print('item: ', item)
+        #print('item: ', item)
         yield item
